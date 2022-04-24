013d50d615f1d5794436f2c5b2a22240361f1ce3
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-04-23 22:08:41 -0500

    Creando clases Escuela, propiedades, constructor, creando objeto guiandonos desde nuestra clase escuela, metodos get y set simplificados

---
+++ POO/POO/Entidades/Escuela.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+
+namespace POO.Entidades
+{
+    class Escuela
+    {
+        //private string _nombre;
+        //public string Nombre { get { return _nombre; } set { _nombre = value.ToUpper(); } }
+        //ENCAPSULAMIENTO
+        public string Nombre { get; set; }
+        public int AnioCreacion { get; set; }
+        public string Pais { get;set; }
+        public string Ciudad { get; set; }
+        //PROP
+        //CONSTRUCTOR
+        //public Escuela(string nombre,int anioCreacion)
+        //{
+        //    Nombre = nombre;
+        //    AnioCreacion = ValidarNumero(anioCreacion);
+        //}
+        public Escuela(string nombre, int anioCreacion)=> (Nombre,AnioCreacion)=(nombre,ValidarNumero(anioCreacion));
+        private int ValidarNumero(int anioCreacion)
+        {
+            if (anioCreacion< 2022)
+            {
+                return anioCreacion;
+            }
+            return 0;
+        }
+    }
+}
