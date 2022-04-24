f76c7621a3712cd56efae755a4f0f068274e8d5e
Laptop Toshiba <santamariaramos18@hotmail.com>
2022-04-23 21:32:01 -0500

    creando clases, propiedades, metodos, modificadores de acceso protected public
     instanciando clase para crear primer objeto

---
+++ POO/POO/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace POO
+{
+    //PLANTILLA
+    class Escuela
+    {
+        public string nombre;
+        public string direccion;
+        public int anioFundacion;
+        public string ceo ="Saul Santamaria";
+
+        //METODO QUE NO RETORNA NADA
+        //void timbrar() => Console.Beep(10000, 3000);
+
+        public void timbrar()
+        {
+            Console.Beep(10000, 1000);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INSTANCIAMOS
+            var miEscuela = new Escuela();
+            miEscuela.nombre = "Los siervos de jesus";
+            string direccion = miEscuela.direccion = "Av Lima 1140";
+            miEscuela.anioFundacion = 2020;
+            Console.WriteLine("Mi escuela va timbrar");
+            miEscuela.timbrar();
+            Console.WriteLine($"El nombre es {miEscuela.nombre}, la direccion es {direccion}");
+
+        }
+    }
+}
